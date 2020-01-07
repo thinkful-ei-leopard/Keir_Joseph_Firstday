4bda6e9ebdad9f05020f2044393f5f9a7acc2aa8
Neruul <viralze@live.com>
2020-01-06 22:13:59 -0800

    added higher-order.js, to be finished early morning.

---
+++ random-js-snippets/higher-order.js
@@ -0,0 +1,14 @@
+function repeat(fn,n){
+for (i = 0; i < n; i++);{
+return fn;
+  }
+function hello(){
+    console.log('Hello world!');
+}
+function goodbye(){
+    console.log('Goodbye world!');
+}
+
+}
+
+//picking up with this early morning
