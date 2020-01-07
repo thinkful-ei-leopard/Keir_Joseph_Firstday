a8d923d03d827fd4e1f5e7d7038b0b9b15ef9416
Neruul <viralze@live.com>
2020-01-06 16:03:39 -0800

    Compared and modified final product.

--- apps.js
+++ apps.js
@@ -3,15 +3,33 @@ let myTeam = 'Joseph and Keir';
 // eslint-console-next-line no-console
 console.log(myTeam);
 
-  function getYearOfBirth(age){
-    return ...
+function getYearOfBirth(age) {
+    return 2020 - age;
   }
-
+  
   function createGreeting(name, age) {
-    const yearOfBirth = 2020 - age;
-    return ;
-
-    const greeting1 = createGreeting();
-    console.log(greeting1);
+    if (name === undefined || age === undefined) {
+      throw new Error('Arguments not valid.');
+    }
+  
+    if (age < 0) {
+    throw new Error('Age cannot be negative.');
 }
   
+    if (typeof age !== 'number') {
+      throw new TypeError('Age must be a number');
+    }
+  
+    const yearOfBirth = getYearOfBirth(age);
+    return `
+      Hi, my name is ${name} and I'm ${age} years old.
+      I was born in ${yearOfBirth}.
+    `;
+  }
+  
+  try {
+    const greeting1 = createGreeting('Joseph', 27);
+    console.log(greeting1);
+  } catch(e) {
+    console.error(e.message);
+  }
