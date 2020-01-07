b3c21686404a6650de65610f6ff51a2b6e2e7a6a
Neruul <viralze@live.com>
2020-01-06 19:31:54 -0800

    Debug

--- apps.js
+++ apps.js
@@ -1,4 +1,35 @@
 // eslint-disable-next-line strict
 let myTeam = 'Joseph and Keir';
-// eslint-disable-next-line no-console
+// eslint-console-next-line no-console
 console.log(myTeam);
+
+function getYearOfBirth(age) {
+    return 2020 - age;
+  }
+  
+  function createGreeting(name, age) {
+    if (name === undefined || age === undefined) {
+      throw new Error('Arguments not valid.');
+    }
+  
+    if (age < 0) {
+    throw new Error('Age cannot be negative.');
+}
+  
+    if (typeof age !== 'number') {
+      throw new TypeError('Age must be a number');
+    }
+  
+    const yearOfBirth = getYearOfBirth(age);
+    return `
+      Hi, my name is ${name} and I'm ${age} years old.
+      I was born in ${yearOfBirth}.
+    `;
+  }
+  
+  try {
+    const greeting1 = createGreeting('Joseph', 27);
+    console.log(greeting1);
+  } catch(e) {
+    console.error(e.message);
+  }
