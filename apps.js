7c16c09844eb5cbd95ef9181e725bb704a4485c3
Neruul <viralze@live.com>
2020-01-06 21:28:44 -0800

    bookmarking

--- apps.js
+++ apps.js
@@ -1,57 +1,60 @@
-// // eslint-disable-next-line strict
-// let myTeam = 'Joseph and Keir';
-// // eslint-console-next-line no-console
-// console.log(myTeam);
+// // // eslint-disable-next-line strict
+// // let myTeam = 'Joseph and Keir';
+// // // eslint-console-next-line no-console
+// // console.log(myTeam);
 
-// function getYearOfBirth(age) {
-//   return 2020 - age;
+// // function getYearOfBirth(age) {
+// //   return 2020 - age;
+// // }
+  
+// // function createGreeting(name, age) {
+// //   if (name === undefined || age === undefined) {
+// //     throw new Error('Arguments not valid.');
+// //   }
+  
+// //   if (age < 0) {
+// //     throw new Error('Age cannot be negative.');
+// //   }
+  
+// //   if (typeof age !== 'number') {
+// //     throw new TypeError('Age must be a number');
+// //   }
+  
+// //   const yearOfBirth = getYearOfBirth(age);
+// //   return `
+// //       Hi, my name is ${name} and I'm ${age} years old.
+// //       I was born in ${yearOfBirth}.
+// //     `;
+// // }
+  
+// // try {
+// //   const greeting1 = createGreeting('Joseph', 27);
+// //   console.log(greeting1);
+// // } catch(e) {
+// //   console.error(e.message);
+// // }
+
+
+
+// function jediName(firstName,lastName){
+//   return lastName.slice(0,3) + firstName.slice(0,2);
 // }
-  
-// function createGreeting(name, age) {
-//   if (name === undefined || age === undefined) {
-//     throw new Error('Arguments not valid.');
+// jediName('Joseph','Bennett');
+
+// function beyond(num){
+//   if (num === Infinity){
+//     console.log('And Beyond');
 //   }
-  
-//   if (age < 0) {
-//     throw new Error('Age cannot be negative.');
+//   else if (num > 0){
+//     console.log('To infinity');
 //   }
-  
-//   if (typeof age !== 'number') {
-//     throw new TypeError('Age must be a number');
+//   else if (num < 0){
+//     console.log('To negative infinity');
 //   }
-  
-//   const yearOfBirth = getYearOfBirth(age);
-//   return `
-//       Hi, my name is ${name} and I'm ${age} years old.
-//       I was born in ${yearOfBirth}.
-//     `;
-// }
-  
-// try {
-//   const greeting1 = createGreeting('Joseph', 27);
-//   console.log(greeting1);
-// } catch(e) {
-//   console.error(e.message);
+//   else if (num === 0){
+//     console.log('Staying home')
+//   }
 // }
 
 
 
-function jediName(firstName,lastName){
-  return lastName.slice(0,3) + firstName.slice(0,2);
-}
-jediName('Joseph','Bennett');
-
-function beyond(num){
-  if (num === Infinity){
-    console.log('And Beyond');
-  }
-  else if (num > 0){
-    console.log('To infinity');
-  }
-  else if (num < 0){
-    console.log('To negative infinity');
-  }
-  else if (num === 0){
-    console.log('Staying home')
-  }
-}
